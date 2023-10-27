59f761c6097c1e02b0433d45072752f22658dbd2
sukmagv <sukmagladysv@gmail.com>
2023-10-27 08:17:48 +0700

    latihan

--- flutter_project_ui/lib/main.dart
+++ flutter_project_ui/lib/main.dart
@@ -5,111 +5,107 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(200, 200, 20, 20),
+            title: const Text("MyApp"),
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  text: 'BERITA TERBARU',
+                ),
+                Tab(
+                  text: 'PERTANDINGAN HARI INI',
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          body: TabBarView(
+            children: [
+              SizedBox(
+                width: 500,
+                height: 300,
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt8f381d19d9c8d246/60dc02d820a5380ed1a4489e/b21af15a62b1f217b7e68c5b183e50f8facb7109.jpg?auto=webp&format=pjpg&width=3840&quality=60',
+                      width: 500,
+                      height: 200,
+                      fit: BoxFit.cover, // Mengatur tampilan gambar
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: const Text(
+                        'Costa Mendekat Ke Palmaeiras', // Teks di bawah gambar
+                        style: TextStyle(
+                          fontSize: 20, // Ukuran teks
+                          fontWeight: FontWeight.bold, // Ketebalan teks
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      alignment: Alignment.centerLeft,
+                      // ignore: sort_child_properties_last
+                      child: const Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ),
+                      height: 50.0,
+                      width: 400.0,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: 3, // Ganti dengan jumlah item yang sesuai
+                        itemBuilder: (context, index) {
+                          const itemImage =
+                              'https://static.bongda24h.vn/medias/standard/2019/4/7/costa.jpg';
+                          const itemTitle =
+                              'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat';
+                          const itemDate = '20-08-2023';
+                          return ListTile(
+                            leading: Image.network(
+                              itemImage,
+                              // Atur lebar gambar sesuai keinginan
+                              width: 250,
+                              height: 250,
+                              fit: BoxFit.cover, // Mengatur tampilan gambar
+                            ),
+                            title: const Text(
+                              itemTitle,
+                              style: TextStyle(
+                                fontSize:
+                                    12.0, // Atur ukuran font sesuai keinginan
+                              ),
+                            ),
+                            subtitle: const Text(
+                              itemDate,
+                              style: TextStyle(
+                                fontSize:
+                                    12.0, // Atur ukuran font sesuai keinginan
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Center(
+                child: Text('Konten Pertandingan Hari ini'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
