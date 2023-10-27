0bb6eaf6ad37a9c2af4b0a5fd57e514f38b0d6f6
sukmagv <sukmagladysv@gmail.com>
2023-10-28 00:26:26 +0700

    progres_2

--- uts/lib/main.dart
+++ uts/lib/main.dart
@@ -1,84 +1,252 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Link Aja',
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-          ),
-          body: Column(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.grey[300], // Warna latar belakang Container
+          child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  FlutterLogo(
-                    size: 30.0,
-                  ),
-                  Text(
-                    "Link Aja",
-                    style: TextStyle(color: Colors.red, fontSize: 30.0),
-                  ),
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                    size: 20.0,
-                  ),
-                ],
-              ),
-              Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 200,
-                  width: 700,
-                  alignment: Alignment.topLeft,
-                  color: Colors.red[600],
-                  child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 10, bottom: 10, left: 20, right: 0),
-                        child: Text(
-                          "Hi, SOFYAN NOOR ARIEF, S.ST, M.KOM",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                          ),
+                      Container(
+                          padding: const EdgeInsets.only(
+                              top: 70, bottom: 20, left: 20, right: 10),
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            width: 50,
+                            height: 50,
+                          )),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        color: Colors.white,
+                        margin: const EdgeInsets.only(
+                            top: 70, bottom: 20, left: 10),
+                        padding: const EdgeInsets.all(5),
+                        child: const Icon(
+                          Icons.confirmation_num_outlined,
+                          size: 40,
                         ),
                       ),
                       Container(
-                          height: 100,
-                          width: 150,
-                          alignment: Alignment.centerLeft,
+                        color: Colors.white,
+                        margin: const EdgeInsets.only(
+                            top: 70, bottom: 20, left: 10, right: 20),
+                        padding: const EdgeInsets.all(5),
+                        child: const Icon(
+                          Icons.star,
+                          size: 40,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.red[700],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                height: 170,
+                margin: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
+                child: Column(children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                        top: 22, bottom: 22, left: 15, right: 15),
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      "Hi, SOFYAN NOOR ARIEF, S.ST, M.KOM",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
                           color: Colors.white,
-                          child: Column(
-                            children: [
-                              Text(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        height: 80,
+                        width: 150,
+                        margin: const EdgeInsets.only(left: 15, right: 15),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  top: 23, bottom: 5, right: 60),
+                              child: const Text(
                                 "Your Balance",
                                 style: TextStyle(
                                   color: Colors.black,
-                                  fontSize: 10.0,
+                                  fontSize: 12.0,
                                 ),
                               ),
-                              Text(
-                                "Rp 10.184",
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(
+                                  bottom: 15,
+                                  left: 10,
+                                ),
+                                child: Row(
+                                  children: const [
+                                    Text(
+                                      "Rp 10.184 ",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_circle_right_rounded,
+                                      size: 15,
+                                      color: Colors.red,
+                                    )
+                                  ],
+                                )),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        height: 80,
+                        width: 150,
+                        // margin: const EdgeInsets.only(left: 15, right: 15),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  top: 23, bottom: 5, left: 10, right: 60),
+                              child: const Text(
+                                "Bonus Balance",
                                 style: TextStyle(
                                   color: Colors.black,
-                                  fontSize: 10.0,
+                                  fontSize: 12.0,
                                 ),
                               ),
-                            ],
-                          ))
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(
+                                  bottom: 15,
+                                  left: 10,
+                                ),
+                                child: Row(
+                                  children: const [
+                                    Text(
+                                      textAlign: TextAlign.start,
+                                      "0 ",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_circle_right_rounded,
+                                      size: 15,
+                                      color: Colors.red,
+                                    )
+                                  ],
+                                )),
+                          ],
+                        ),
+                      ),
                     ],
-                  )),
+                  )
+                ]),
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  margin: const EdgeInsets.only(
+                      top: 10, bottom: 10, left: 20, right: 20),
+                  padding: const EdgeInsets.only(top: 12, bottom: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: const [
+                          Icon(Icons.star, size: 40),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            "TopUp",
+                            style: TextStyle(fontSize: 13),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Icon(Icons.star, size: 40),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            "TopUp",
+                            style: TextStyle(fontSize: 13),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Icon(Icons.star, size: 40),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            "TopUp",
+                            style: TextStyle(fontSize: 13),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Icon(Icons.star, size: 40),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            "TopUp",
+                            style: TextStyle(fontSize: 13),
+                          )
+                        ],
+                      )
+                    ],
+                  ))
             ],
           )),
     );
